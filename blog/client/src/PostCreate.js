6ff3bed3520dae5088c0805f380aa367581a32c8
vladilin <ilin96inbox@gmail.com>
2020-05-28 19:56:00 +0900

    finish blog app

--- blog/client/src/PostCreate.js
+++ blog/client/src/PostCreate.js
@@ -1,15 +1,19 @@
-import React, { useState } from "react";
-import axios from "axios";
+import React, { useState } from 'react';
+import axios from 'axios';
 
 export default () => {
-  const [title, setTitle] = useState("");
-  const onSubmit = async (event) => {
+  const [title, setTitle] = useState('');
+
+  const onSubmit = async event => {
     event.preventDefault();
-    await axios.post("http://localhost:4000/posts", {
-      title,
+
+    await axios.post('http://localhost:4000/posts', {
+      title
     });
-    setTitle("");
+
+    setTitle('');
   };
+
   return (
     <div>
       <form onSubmit={onSubmit}>
@@ -17,9 +21,9 @@ export default () => {
           <label>Title</label>
           <input
             value={title}
-            onChange={(e) => setTitle(e.target.value)}
+            onChange={e => setTitle(e.target.value)}
             className="form-control"
-          ></input>
+          />
         </div>
         <button className="btn btn-primary">Submit</button>
       </form>
